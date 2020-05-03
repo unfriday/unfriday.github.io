59e0ac83678bbed3ec757b59628f6c817815b720
unfriday <64720891+unfriday@users.noreply.github.com>
2020-05-03 11:10:50 +0600

    Add files via upload

---
+++ madscientist.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>madscientist</title>
+<link rel="SHORTCUT ICON" href="https://lh5.googleusercontent.com/nbMcakeOJ_B7U6B2IKFIgeoStD5xdSWuB7iALEPluyywqO5elDiMDMPh2_w=w2400" type="image/x-icon"/>
+<link href='https://fonts.googleapis.com/css?family=Nosifer' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Sarpanch:700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=play" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://csshake.surge.sh/csshake.min.css"><link rel="stylesheet" href="./madscientist/style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+
+<div class="stars">
+  <center>
+  </center>
+</div>
+<div class="twinkling">
+  <center><br><br><br>
+    <img src="https://lh4.googleusercontent.com/EkVUQ7uu6-Qd-w1RQeEjFtGSkMjq750v_ykraMz-CJ5-9NYEW6GoUmQqz_o=w2400" width="250"/><br>
+    <div class="container">
+      <div class="text"></div>
+    </div><br><br><br>
+    <font face="Sarpanch" color="white" size"10" class="message">
+      |&nbsp;  The person who make it does not as smart as the person who <font color="red">Hacked </font>it!&nbsp;  |
+    </font><br>
+    <font face="Sarpanch" color="white" size"10" class="message">#madscientist &bull; Join Us <font color="red">**)!#!)!*&&^&.</font><br><br>
+    
+  </center>
+</div>
+<div class="clouds">
+</div>
+<iframe width="1" height="1" src="https://www.youtube.com/embed/LApS9G22cIU?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+<!-- partial -->
+  <script  src="./madscientist/script.js"></script>
+
+</body>
+</html>
